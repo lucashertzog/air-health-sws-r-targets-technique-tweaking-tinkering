e3b4d28292451ae242ffcda95e48bd9f09b91aa1
ivanhanigan <ivan.hanigan@gmail.com>
2023-03-10 21:09:19 +0800

    mods

--- example_02_a_simple_targets_pipeline/main.R
+++ example_02_a_simple_targets_pipeline/main.R
@@ -1,5 +1,7 @@
 library(targets)
 
+if(!file.exists("figures_and_tables")) dir.create("figures_and_tables")
+
 sapply(dir("R/", pattern = ".R$", full.names = T), source)
 
 tar_visnetwork(targets_only = T)
